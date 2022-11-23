5a2294e8f34b52fd95c906e4470eecb3d6aab13c
37494 <37494@GWTN156-1>
2022-11-23 21:00:05 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //data Tape     data name     value 
+        byte byteType=25;
+        Console.WriteLine(byteType);
+        
+
+    }
+}
