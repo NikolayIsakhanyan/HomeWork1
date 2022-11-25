dc9fdc5fdcc5594c88affe8001dfa2c420428bb5
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-11-25 18:30:22 +0400

    HomeWork

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,11 +1,21 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        //data Tape     data name     value 
-        byte byteType=25;
-        Console.WriteLine(byteType);
-        
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
-    }
-}
+//float a = 3.1239568f;  a=3.123957
+//float a = 333.1239568f;  a=333.123962
+//Console.WriteLine(a);  TviMetsutyunicKakhvats
+//string a;
+//a = "text";
+//Console.WriteLine(a);
+StringBuilder str = new StringBuilder();
+str.Append("barev ");
+str.Append(" hajogh");
+Console.WriteLine(str);//Veragruma Ham "barev" Ham "hajogh"
+//string sa = str.ToString();
+//string sb= str.ToString();
+
+//    Console.WriteLine(sb);
+//Console.WriteLine(sa);
+string a = "barev";
+ a = "hajogh";
+Console.WriteLine(a);
