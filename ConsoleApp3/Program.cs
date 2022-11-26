ff07d7876953cff520373ed6323aca7a611d4505
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-11-26 12:22:44 +0400

    ClassWork

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,15 +7,72 @@ using System.Text;
 //string a;
 //a = "text";
 //Console.WriteLine(a);
-StringBuilder str = new StringBuilder();
-str.Append("barev ");
-str.Append(" hajogh");
-Console.WriteLine(str);//Veragruma Ham "barev" Ham "hajogh"
+//StringBuilder str = new StringBuilder();
+//str.Append("barev ");  
+//str.Append(" hajogh");
+//str.Replace("a", "b"); tuny nayem
+//Console.WriteLine(str);//Veragruma Ham "barev" Ham "hajogh"
 //string sa = str.ToString();
 //string sb= str.ToString();
 
 //    Console.WriteLine(sb);
 //Console.WriteLine(sa);
-string a = "barev";
- a = "hajogh";
-Console.WriteLine(a);
+//string a = "barev";
+// a = "hajogh";
+//Console.WriteLine(a);
+//long a=2;
+//int b = 3;
+//long c = a + b;
+//Console.WriteLine(c);
+//int i = 1;
+//i++;
+//int j = 1;
+////++j;
+//Console.WriteLine(i);
+//Console.WriteLine(j);
+//int k = 1;
+//Console.Write(k);
+
+//++k;
+//Console.WriteLine(k);
+//k++;
+//Console.WriteLine(k);
+//int a = 5;
+//int b = 4;
+//if (a > b)
+//{ Console.WriteLine("true"); }
+//else if (a < b)
+//    {
+
+//        Console.WriteLine("false");
+//    {
+//int a = 5;
+//switch (a)
+//{
+//	case 1:
+//		Console.WriteLine("2");
+//		break;
+//	case 2:
+//		Console.WriteLine("5");
+//		break;
+//	case 3:
+//		Console.WriteLine("6");
+//		break;
+//	case 4:
+//		Console.WriteLine("3");
+//		break;
+//	case 5:
+//		Console.WriteLine("4");
+//		break;
+//	default:
+//		Console.WriteLine("verjnakan ");//elsi nman
+//		break
+int sum = 0;
+for(int i = 0; i <= 100; i = i + 2) {
+    sum += i;
+}
+Console.WriteLine(sum); 
+
+
+
+
